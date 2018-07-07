d633678507cd7a4025ca6302bd11cad4524a46b6
Jasper Ginn <jasperginn@gmail.com>
2018-07-07 15:54:58 +0200

    Add function to load variables on startup

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+# --------------------------------------------------------------------------
+#
+# zzz.R is evaluated when the package is loaded
+#
+# --------------------------------------------------------------------------
+
+# When the package is loaded, do ...
+.onLoad <- function(libname = find.package("FinTxtClient"), pkgname="FinTxtClient") {
+
+  # Set options for retinaSDK python module
+  if(Sys.getenv("FINTXT_CLIENT_URL") == "") {
+    Sys.setenv("FINTXT_CLIENT_URL" = "http://127.0.01:8000")
+  }
+
+}
