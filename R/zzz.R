7daf927af2244c1f090e61cea21da5af386bdc39
Jasper Ginn <jasperginn@gmail.com>
2018-07-19 14:06:46 +0200

    http --> https

--- R/zzz.R
+++ R/zzz.R
@@ -7,9 +7,9 @@
 # When the package is loaded, do ...
 .onLoad <- function(libname = find.package("FinTxtClient"), pkgname="FinTxtClient") {
 
-  # Set options for retinaSDK python module
+  # Set options for FinTxtClient package
   if(Sys.getenv("FINTXT_CLIENT_URL") == "") {
-    Sys.setenv("FINTXT_CLIENT_URL" = "http://api.fintxt.io/rest")
+    Sys.setenv("FINTXT_CLIENT_URL" = "https://api.fintxt.io/rest")
   }
 
 }
